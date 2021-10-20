6e99caad70b628ea84552b0c9717b65ff71771f4
liyajie <liyajie@rytad.com>
2021-10-20 10:44:27 +0800

    n的阶乘

--- recur/pow.go
+++ recur/pow.go
@@ -8,10 +8,10 @@ func MyPow(x float64, n int) float64 {
 	}
 
 	if n < 0 {
-		return 1.0/myPow(x, -n)
+		return 1.0/MyPow(x, -n)
 	}
 
-	temp := myPow(x, int(math.Ceil(float64(n/2))))
+	temp := MyPow(x, int(math.Ceil(float64(n/2))))
 	ans := temp * temp
 	if n % 2 == 1 {
 		ans *= x
